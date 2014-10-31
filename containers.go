39566a718b4f52433d223a8b929669d449c87ecc
Michael Crosby <crosbymichael@gmail.com>
2014-10-31 15:17:18 -0700

    Add state and Dockerfile

--- containers.go
+++ containers.go
@@ -12,7 +12,7 @@ func loadContainers(client *dockerclient.DockerClient, db *sql.DB) error {
 		return err
 	}
 	if _, err := db.Exec(
-		"CREATE TABLE containers (id, image, name, status, command, cpu, memory, cpuset, ip)"); err != nil {
+		"CREATE TABLE containers (id, image, name, state, command, cpu, memory, cpuset, ip)"); err != nil {
 		return err
 	}
 	for _, c := range containers {
@@ -22,10 +22,27 @@ func loadContainers(client *dockerclient.DockerClient, db *sql.DB) error {
 		}
 
 		if _, err := db.Exec(
-			"INSERT INTO containers (id, image, name, status, command, cpu, memory, cpuset, ip) VALUES (?, ?, ?, ? ,?, ?, ?, ?, ?)",
-			c.Id, c.Image, c.Names[0], c.Status, c.Command, info.Config.CpuShares, info.Config.Memory, info.Config.Cpuset, info.NetworkSettings.IpAddress); err != nil {
+			`INSERT INTO containers 
+            (id, image, name, state, command, cpu, memory, cpuset, ip) 
+            VALUES (?, ?, ?, ? ,?, ?, ?, ?, ?)`,
+			c.Id, c.Image, info.Name, getState(info), c.Command, info.Config.CpuShares, info.Config.Memory,
+			info.Config.Cpuset, info.NetworkSettings.IpAddress); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func getState(info *dockerclient.ContainerInfo) string {
+	s := info.State
+	switch {
+	case s.Paused:
+		return "paused"
+	case s.Running:
+		return "running"
+	case s.Restarting:
+		return "restarting"
+	default:
+		return "stopped"
+	}
 }
